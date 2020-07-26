9f64c2de133e8c5d0c9b71b51e6010df05c20379
Koboti Mogale <coffart@gmail.com>
2020-07-26 21:50:24 +0200

    Added maven framework support and spring boot dependency

---
+++ src/test/java/co/za/koboti/training/playsafe/conversion/ConversionApplicationTest.java
@@ -0,0 +1,11 @@
+package co.za.koboti.training.playsafe.conversion;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConversionApplicationTest {
+
+}
